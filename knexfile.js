ac1a5579969005888318120118feeb093ef3a98b
rob kurtz <rob@robkurtz.net>
2017-05-04 13:41:55 -0600

    redid env vars for aws

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,12 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: {
+      database: process.env.DB,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+      host: process.env.DB_HOST
+    }
   }
 };
